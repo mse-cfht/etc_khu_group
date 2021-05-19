4c24bf4b9a5ca581ea5251be0623431801cbddba
Tae-Geun <jtg777@khu.ac.kr>
2021-05-19 17:12:12 +0900

    Update MSE-ETC v1.0.0

--- mse_etc/output.py
+++ mse_etc/output.py
@@ -7,9 +7,11 @@ Modification Log:
     * 2021.03.24 - Updated by Tae-Geun Ji
     * 2021.03.29 - Updated by Tae-Geun Ji
     * 2021.04.09 - Updated by Hojae Ahn
+    * 2021.05.18 - Updated by Changgon Kim
 """
 
 from parameters import *
+from initial_values import *
 from pylab import *
 import matplotlib.pyplot as plt
 
@@ -36,6 +38,41 @@ def display_single(res_mode, pwv, exp_t, exp_n, mag, sky, data, wave):
 
         if RES_LR[5] != -1:
             print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_LR[5]))
+
+    elif res_mode == "MR":
+
+        print('Resolution Mode   = Moderate Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print(' ')
+        print('Band\t Mag. \t Sky \t S/N')
+        print('[Blue]\t %.2f \t %.2f \t %f' % (mag[0], sky[0], data[0]))
+        print('[Green]\t %.2f \t %.2f \t %f' % (mag[1], sky[1], data[1]))
+        print('[Red]\t %.2f \t %.2f \t %f' % (mag[2], sky[2], data[2]))
+        print('[NIR]\t %.2f \t %.2f \t %f' % (mag[3], sky[3], data[3]))
+        print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[4], sky[4], data[4], BAND_MR[4]))
+
+        if RES_MR[5] != -1:
+            print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_MR[5]))
+
+    elif res_mode == "HR":
+
+        print('Resolution Mode   = High Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print(' ')
+        print('Band\t Mag. \t Sky \t S/N')
+        print('[Blue]\t %.2f \t %.2f \t %f' % (mag[0], sky[0], data[0]))
+        print('[Green]\t %.2f \t %.2f \t %f' % (mag[1], sky[1], data[1]))
+        print('[Red]\t %.2f \t %.2f \t %f' % (mag[2], sky[2], data[2]))
+        #print('[NIR]\t %.2f \t %.2f \t %f' % (mag[3], sky[3], data[3]))
+        print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[4], sky[4], data[4], BAND_HR[4]))
+
+        if RES_HR[5] != -1:
+            print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_HR[5]))
+
 
 
 def display_simple_text(text):
@@ -65,6 +102,41 @@ def display_exp_time(res_mode, pwv, target_sn, mag, sky, data, wave):
         if RES_LR[5] != -1:
             print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_LR[5]))
 
+    elif res_mode == "MR":
+
+        print('Resolution Mode   = Moderate Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Number   = 1')
+        print('Target S/N   = %d' % target_sn)
+        print(' ')
+        print('Band\t Mag. \t Sky \t ExpTime [s]')
+        print('[Blue]\t %.2f \t %.2f \t %f' % (mag[0], sky[0], data[0]))
+        print('[Green]\t %.2f \t %.2f \t %f' % (mag[1], sky[1], data[1]))
+        print('[Red]\t %.2f \t %.2f \t %f' % (mag[2], sky[2], data[2]))
+        print('[NIR]\t %.2f \t %.2f \t %f' % (mag[3], sky[3], data[3]))
+        print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[4], sky[4], data[4], BAND_MR[4]))
+
+        if RES_LR[5] != -1:
+            print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_MR[5]))
+
+    elif res_mode == "HR":
+
+        print('Resolution Mode   = High Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Number   = 1')
+        print('Target S/N   = %d' % target_sn)
+        print(' ')
+        print('Band\t Mag. \t Sky \t ExpTime [s]')
+        print('[Blue]\t %.2f \t %.2f \t %f' % (mag[0], sky[0], data[0]))
+        print('[Green]\t %.2f \t %.2f \t %f' % (mag[1], sky[1], data[1]))
+        print('[Red]\t %.2f \t %.2f \t %f' % (mag[2], sky[2], data[2]))
+        #print('[NIR]\t %.2f \t %.2f \t %f' % (mag[3], sky[3], data[3]))
+        print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[4], sky[4], data[4], BAND_HR[4]))
+
+        if RES_HR[5] != -1:
+            print('[%.1f]\t %.2f \t %.2f \t %f \t (Band = %s)' % (wave, mag[5], sky[5], data[5], BAND_HR[5]))
+
+
 def display_sn_mag(res_mode, pwv, exp_t, exp_n, min_mag, max_mag, mag_range, sky, result):
 
     print('==========================================================================')
@@ -91,7 +163,7 @@ def display_sn_mag(res_mode, pwv, exp_t, exp_n, min_mag, max_mag, mag_range, sky
         ax.plot(mag_range, result[0], 'b', mag_range, result[1], 'g',
                 mag_range, result[2], 'r', mag_range, result[3], 'black', linewidth=1)
 
-        plt.title('ETC version 0.2.0 (t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',fontsize=16)
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',fontsize=16)
         plt.xlabel('Point Target Magnitude (AB)', fontsize=15)
         plt.ylabel('Signal-to-Noise',fontsize=15)
         plt.legend(['Blue_LR', 'Green_LR', 'Red_LR', 'NIR_LR'], fontsize=15)
@@ -100,6 +172,80 @@ def display_sn_mag(res_mode, pwv, exp_t, exp_n, min_mag, max_mag, mag_range, sky
         plt.setp(labels, 'fontsize', 'large')
         locs, labels = yticks()
         plt.setp(labels,'fontsize', 'large')
+
+        ax.set_yscale('log')
+        ax.axis([min_mag, max_mag, 1, 1000])
+        ax.grid(color='k', linestyle='-', which='minor', linewidth=0.5)
+        ax.grid(color='k', linestyle='-', which='major', linewidth=1)
+
+        plt.show()
+
+    elif res_mode == "MR":
+
+        print('Resolution Mode   = Moderate Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print('Magnitude Range [mag] : %.2f' % min_mag + ' - ' + '%.2f' % max_mag)
+        print(' ')
+        print('Band\t Sky')
+        print('[Blue]\t %.2f' % (sky[0]))
+        print('[Green]\t %.2f' % (sky[1]))
+        print('[Red]\t %.2f' % (sky[2]))
+        print('[NIR]\t %.2f' % (sky[3]))
+
+        plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+
+        ax = plt.subplot(111)
+        ax.plot(mag_range, result[0], 'b', mag_range, result[1], 'g',
+                mag_range, result[2], 'r', mag_range, result[3], 'black', linewidth=1)
+
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')', fontsize=16)
+        plt.xlabel('Point Target Magnitude (AB)', fontsize=15)
+        plt.ylabel('Signal-to-Noise', fontsize=15)
+        plt.legend(['Blue_MR', 'Green_MR', 'Red_MR', 'NIR_MR'], fontsize=15)
+
+        locs, labels = xticks()
+        plt.setp(labels, 'fontsize', 'large')
+        locs, labels = yticks()
+        plt.setp(labels, 'fontsize', 'large')
+
+        ax.set_yscale('log')
+        ax.axis([min_mag, max_mag, 1, 1000])
+        ax.grid(color='k', linestyle='-', which='minor', linewidth=0.5)
+        ax.grid(color='k', linestyle='-', which='major', linewidth=1)
+
+        plt.show()
+
+    elif res_mode == "HR":
+
+        print('Resolution Mode   = High Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print('Magnitude Range [mag] : %.2f' % min_mag + ' - ' + '%.2f' % max_mag)
+        print(' ')
+        print('Band\t Sky')
+        print('[Blue]\t %.2f' % (sky[0]))
+        print('[Green]\t %.2f' % (sky[1]))
+        print('[Red]\t %.2f' % (sky[2]))
+        print('[NIR]\t %.2f' % (sky[3]))
+
+        plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+
+        ax = plt.subplot(111)
+        ax.plot(mag_range, result[0], 'b', mag_range, result[1], 'g',
+                mag_range, result[2], 'r', mag_range, result[3], 'black', linewidth=1)
+
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')', fontsize=16)
+        plt.xlabel('Point Target Magnitude (AB)', fontsize=15)
+        plt.ylabel('Signal-to-Noise', fontsize=15)
+        plt.legend(['Blue_HR', 'Green_HR', 'Red_HR'], fontsize=15)
+
+        locs, labels = xticks()
+        plt.setp(labels, 'fontsize', 'large')
+        locs, labels = yticks()
+        plt.setp(labels, 'fontsize', 'large')
 
         ax.set_yscale('log')
         ax.axis([min_mag, max_mag, 1, 1000])
@@ -140,7 +286,7 @@ def display_sn_wave(res_mode, wave_mode, pwv, exp_t, exp_n, mag, sky, min_wave, 
             ax.plot(wave_arr, sn_arr, 'black', linewidth=1)
             plt.legend([wave_mode], fontsize=15)
 
-        plt.title('ETC version 0.2.0 (pwv=' + '%.1f' % pwv + ', t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(pwv=' + '%.1f' % pwv + ', t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',
                       fontsize=16)
         plt.xlabel('Wavelength', fontsize=15)
         plt.ylabel('SNR', fontsize=15)
@@ -158,37 +304,93 @@ def display_sn_wave(res_mode, wave_mode, pwv, exp_t, exp_n, mag, sky, min_wave, 
         ax.grid(color='k', linestyle='-', which='major', linewidth=1)
 
         plt.show()
-        """
+
+    elif res_mode == "MR":
+
+        print('Resolution Mode   = Moderate Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print('Magnitude = %.2f' % mag)
+        print('Sky = %.2f' % sky)
+        print('Calculated Wavelength Range [nm] : %.2f' % min_wave + ' - ' + '%.2f' % max_wave)
+        print(' ')
+
+        plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+
+        ax = plt.subplot(111)
+
+        if wave_mode == "Input Wave":
+            ax.plot(wave_arr[0], sn_arr[0], 'blue', linewidth=1, label='Blue')
+            ax.plot(wave_arr[1], sn_arr[1], 'green', linewidth=1, label='Green')
+            ax.plot(wave_arr[2], sn_arr[2], 'red', linewidth=1, label='Red')
+            ax.plot(wave_arr[3], sn_arr[3], 'black', linewidth=1, label='NIR')
+
+            plt.legend(fontsize=15)
         else:
-            print('Resolution Mode   = Low Resolution')
-            print('PWV [mm]          = %.1f' % pwv)
-            print('Exposure Time [s] = %d' % exp_t)
-            print('Exposure Number   = %d' % exp_n)
-            print('Magnitude = %.2f' % mag)
-            print('Sky = %.2f' % sky)
-            print('Calculated Wavelength Range [nm] : %.2f' % min_wave + ' - ' + '%.2f' % max_wave)
-            print(' ')
-
-            plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
-
-            ax = plt.subplot(111)
             ax.plot(wave_arr, sn_arr, 'black', linewidth=1)
-
-            plt.title('ETC version 0.2.0 (pwv=' + '%.1f' % pwv + ', t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',
-                      fontsize=16)
-            plt.xlabel('Wavelength', fontsize=15)
-            plt.ylabel('SNR',fontsize=15)
             plt.legend([wave_mode], fontsize=15)
 
-            locs, labels = xticks()
-            plt.setp(labels, 'fontsize', 'large')
-            locs, labels = yticks()
-            plt.setp(labels,'fontsize', 'large')
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(pwv=' + '%.1f' % pwv + ', t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',
+                  fontsize=16)
+        plt.xlabel('Wavelength', fontsize=15)
+        plt.ylabel('SNR', fontsize=15)
 
-            #ax.set_yscale('log')
-            plt.xlim([min_wave, max_wave])
-            ax.grid(color='k', linestyle='-', which='minor', linewidth=0.5)
-            ax.grid(color='k', linestyle='-', which='major', linewidth=1)
+        # plt.legend([wave_mode], fontsize=15)
 
-            plt.show()
-        """
+        locs, labels = xticks()
+        plt.setp(labels, 'fontsize', 'large')
+        locs, labels = yticks()
+        plt.setp(labels, 'fontsize', 'large')
+
+        # ax.set_yscale('log')
+        plt.xlim([min_wave, max_wave])
+        ax.grid(color='k', linestyle='-', which='minor', linewidth=0.5)
+        ax.grid(color='k', linestyle='-', which='major', linewidth=1)
+
+        plt.show()
+
+    elif res_mode == "HR":
+
+        print('Resolution Mode   = High Resolution')
+        print('PWV [mm]          = %.1f' % pwv)
+        print('Exposure Time [s] = %d' % exp_t)
+        print('Exposure Number   = %d' % exp_n)
+        print('Magnitude = %.2f' % mag)
+        print('Sky = %.2f' % sky)
+        print('Calculated Wavelength Range [nm] : %.2f' % min_wave + ' - ' + '%.2f' % max_wave)
+        print(' ')
+
+        plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+
+        ax = plt.subplot(111)
+
+        if wave_mode == "Input Wave":
+            ax.plot(wave_arr[0], sn_arr[0], 'blue', linewidth=1, label='Blue')
+            ax.plot(wave_arr[1], sn_arr[1], 'green', linewidth=1, label='Green')
+            ax.plot(wave_arr[2], sn_arr[2], 'red', linewidth=1, label='Red')
+            #ax.plot(wave_arr[3], sn_arr[3], 'black', linewidth=1, label='NIR')
+
+            plt.legend(fontsize=15)
+        else:
+            ax.plot(wave_arr, sn_arr, 'black', linewidth=1)
+            plt.legend([wave_mode], fontsize=15)
+
+        plt.title('MSE-ETC' + ' ' + ini_etc_version + ' ' + '(pwv=' + '%.1f' % pwv + ', t=' + '%d' % exp_t + 's, N=' + '%d' % exp_n + ')',
+                  fontsize=16)
+        plt.xlabel('Wavelength', fontsize=15)
+        plt.ylabel('SNR', fontsize=15)
+
+        # plt.legend([wave_mode], fontsize=15)
+
+        locs, labels = xticks()
+        plt.setp(labels, 'fontsize', 'large')
+        locs, labels = yticks()
+        plt.setp(labels, 'fontsize', 'large')
+
+        # ax.set_yscale('log')
+        plt.xlim([min_wave, max_wave])
+        ax.grid(color='k', linestyle='-', which='minor', linewidth=0.5)
+        ax.grid(color='k', linestyle='-', which='major', linewidth=1)
+
+        plt.show()
--- mse_etc/parameters.py
+++ mse_etc/parameters.py
@@ -19,9 +19,19 @@ D_TEL = 10.14           # Telescope aperture [m]
 S_ZM = 10**(-56.1/2.5)  # Definition of AB mag [W m-2 Hz-2]
 N_RES = 16              # Pixel
 N_DARK = 0.02           # Dark current [e s-1 pix-1]
+# ==== LR (added by CK)
 RES_LR = [2550, 3650, 3600, 3600, 1, -1]  # B, G, R, NIR, and Input band in LR: spectral resolution [nm]
 WAVE_LR = [482, 626, 900, 1235, 0, 0]     # B, G, R, NIR, and Input band in LR: wavelength [nm]
 N_READ_LR = [13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 21.4/math.sqrt(7.2), 0, 0]
+# ==== MR (added by CK)
+RES_MR = [4400, 6200, 6100, 6000, 1, -1]  # B, G, R, NIR, and Input band in LR: spectral resolution [nm]
+WAVE_MR = [482, 626, 767, 1662, 0, 0]     # B, G, R, NIR, and Input band in LR: wavelength [nm]
+N_READ_MR = [13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 21.4/math.sqrt(7.2), 0, 0]
+# ==== HR (added by CK)
+RES_HR = [40000, 40000, 20000, 20000, 1, -1]  # B, G, R, NIR, and Input band in LR: spectral resolution [nm]
+WAVE_HR = [400, 482, 767, 900, 0, 0]     # B, G, R, NIR, and Input band in LR: wavelength [nm]
+N_READ_HR = [13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 13.4/math.sqrt(7.2), 21.4/math.sqrt(7.2), 0, 0]
+
 
 # ==== Throughput Parameters: B, G, R, and NIR band of LR
 ENCL_LR = 1.000
@@ -35,5 +45,33 @@ SIP_LR = [0.496, 0.534, 0.543, 0.478]
 TAU_IE_LR = [0.604, 0.607, 0.648, 0.689]         # Injection efficiency
 TAU_ATMO_LR = [1.000, 1.000, 1.000, 1.000]       # Atmospheric transmission (TBD)
 
+# ==== Throughput Parameters: B, G, R, and NIR band of MR (added by CK)
+ENCL_MR = 1.000
+TEL_MSTR_MR = 0.960
+TEL_M1_ZECOAT_MR = [0.937, 0.954, 0.954, 0.963]
+TEL_PFHS_MR = 0.987
+TEL_WFC_ADC_MR = [0.856, 0.803, 0.824, 0.675]
+SIP_POSS_MR = 0.970
+SIP_FITS_MR = [0.784, 0.849, 0.865, 0.810]
+SIP_MR_WG = [0.405, 0.483, 0.448, 0]
+SIP_MR_WoG = [0.560, 0.608, 0.628, 0]
+TAU_IE_MR = [0.606, 0.612, 0.633, 0.574]         # Injection efficiency
+TAU_ATMO_MR = [1.000, 1.000, 1.000, 1.000]       # Atmospheric transmission (TBD)
+
+# ==== Throughput Parameters: B, G, R, and NIR band of MR (added by CK)
+ENCL_HR = 1.000
+TEL_MSTR_HR = 0.960
+TEL_M1_ZECOAT_HR = [0.959, 0.937, 0.954, 0]
+TEL_PFHS_HR = 0.987
+TEL_WFC_ADC_HR = [0.767, 0.856, 0.824, 0]
+SIP_POSS_HR = 0.970
+SIP_FITS_HR = [0.596, 0.742, 0.875, 0]
+SIP_HR_WG = [0.405, 0.354, 0.435, 0]
+SIP_HR_WoG = [0.405, 0.415, 0.435, 0]
+TAU_IE_HR = [0.426, 0.430, 0.449, 0]         # Injection efficiency
+TAU_ATMO_HR = [1.000, 1.000, 1.000, 1.000]       # Atmospheric transmission (TBD)
+
 # ==== Text Paramters
 BAND_LR = ['Blue', 'Green', 'Red', 'NIR', '', '']
+BAND_MR = ['Blue', 'Green', 'Red', 'NIR', '', '']
+BAND_HR = ['Blue', 'Green', 'Red', '', '', '']
